f99d118dbcca758d5d6dbaa8f9c47278eb6ba6d1
Ptonmi2016 <ocwcheng@gmail.com>
2017-12-22 18:52:35 +0100

    setup server and client conection

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,11 @@
+var socket;
+
 function setup() {
   // put setup code here
   createCanvas(500,500);
   background(51);
+
+  socket = io.connect('http://localhost:1000/');
 }
 
 function draw() {
--- server.js
+++ server.js
@@ -6,3 +6,14 @@ var server = app.listen(1000);
 app.use(express.static('public'))
 
 console.log("server is ready!")
+
+var socket = require('socket.io');
+
+var io = socket(server); 
+
+io.on('connection', newConnection);
+
+function newConnection(socket){
+	console.log("new connection: " + socket.id);
+}
+
